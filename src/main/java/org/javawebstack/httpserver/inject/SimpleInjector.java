26627a3d9702a28a2308b603248161e11c9032e5
JanHolger <jan@bebendorf.eu>
2020-11-04 19:05:22 +0100

    Made SimpleInjector work recursively

--- src/main/java/org/javawebstack/httpserver/inject/SimpleInjector.java
+++ src/main/java/org/javawebstack/httpserver/inject/SimpleInjector.java
@@ -9,6 +9,13 @@ public class SimpleInjector implements Injector {
     private final Map<Class<?>, Object> instances = new HashMap<>();
 
     public <T> T inject(T object) {
+        inject(object.getClass(), object);
+        return object;
+    }
+
+    private void inject(Class<?> clazz, Object object){
+        if(!Object.class.equals(clazz.getSuperclass()))
+            inject(clazz.getSuperclass(), object);
         for(Field field : object.getClass().getDeclaredFields()){
             if(field.getDeclaredAnnotationsByType(Inject.class).length != 0){
                 if(instances.containsKey(field.getType())){
@@ -19,7 +26,6 @@ public class SimpleInjector implements Injector {
                 }
             }
         }
-        return object;
     }
 
     public <T> T getInstance(Class<T> clazz) {
