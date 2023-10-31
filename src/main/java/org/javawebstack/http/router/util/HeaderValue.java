ba03cde4692913e53a9f7857d8f58b3c0329fdce
Jake Esser <j.esser@lumaserv.com>
2023-10-31 17:26:54 +0100

    Removed space from HeaderValue parsing and added trimming

--- src/main/java/org/javawebstack/http/router/util/HeaderValue.java
+++ src/main/java/org/javawebstack/http/router/util/HeaderValue.java
@@ -9,10 +9,10 @@ public class HeaderValue {
     private final Map<String, String> directives = new HashMap<>();
 
     public HeaderValue(String source) {
-        String[] spl = source.split("; ");
-        value = spl[0];
+        String[] spl = source.split(";");
+        value = spl[0].trim();
         for(int i=1; i<spl.length; i++) {
-            String[] spl2 = spl[i].split("=", 2);
+            String[] spl2 = spl[i].trim().split("=", 2);
             String value = spl2.length == 2 ? spl2[1] : "";
             if(value.startsWith("\"") && value.endsWith("\"")) {
                 value = value.substring(1, value.length()-1);
